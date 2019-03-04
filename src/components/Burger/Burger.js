1ce5a9c79ff0906ee98c3a117b6c434f835a27a1
isjaki <usow.aleksey@yandex.ru>
2019-03-04 18:49:41 +0400

    added state with ingredients to the "BurgerBuilder" component; added a method to output "BurgerIngredient" components dynamically, depending on the amount of ingrediends in the state of "BurgerBuilder" component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingredient => {
+            return [...Array(props.ingredients[ingredient])].map((_, index) => {
+                return <BurgerIngredient key={ingredient + index} type={ingredient} />;
+            });
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
