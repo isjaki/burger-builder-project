77e87610b3ccaf040872d418e9b98fbe36bcd249
isjaki <usow.aleksey@yandex.ru>
2019-03-05 13:44:16 +0400

    added a paragraph with a total price which is shown above the controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -16,8 +16,8 @@ class BurgerBuilder extends Component {
         ingredients: {
             salad: 0,
             bacon: 0,
-            meat: 0,
-            cheese: 0
+            cheese: 0,
+            meat: 0
         },
         totalPrice: 4
     }
@@ -78,7 +78,8 @@ class BurgerBuilder extends Component {
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo} />
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice} />
             </Aux>
         );
     }
