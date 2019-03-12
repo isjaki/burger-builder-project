9c1a94977405fb85de43ef29600bf2054f32626c
isjaki <usow.aleksey@yandex.ru>
2019-03-12 22:45:07 +0400

    now we are storing the ingredients object in the database and retrieve it from the server when loading the page, showing spinner when ingredients are being loaded; added error handling in case when the get request doesn't work

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -5,21 +5,23 @@ import Aux from '../Auxiliary/Auxiliary';
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
-        state = {
-            error: null
-        }
+        constructor(props) {
+            super(props);
 
-        componentDidMount() {
             axios.interceptors.request.use(request => {
                 this.setState({ error: null });
                 return request;
             });
-        
+
             axios.interceptors.response.use(response => response, error => {
                 this.setState({ error: error });
             });
         }
 
+        state = {
+            error: null
+        }
+        
         errorConfirmedHandler = () => {
             this.setState({ error: null });
         }
