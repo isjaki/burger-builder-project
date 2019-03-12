72337ef7e270d5681c89f2f34d59c383c353c609
isjaki <usow.aleksey@yandex.ru>
2019-03-13 00:19:32 +0400

    added componentWillUnmount hook to "withErrorHandler" component to remove reduundant interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,12 +8,12 @@ const withErrorHandler = (WrappedComponent, axios) => {
         constructor(props) {
             super(props);
 
-            axios.interceptors.request.use(request => {
+            this.reqInterceptor = axios.interceptors.request.use(request => {
                 this.setState({ error: null });
                 return request;
             });
 
-            axios.interceptors.response.use(response => response, error => {
+            this.resInterceptor = axios.interceptors.response.use(response => response, error => {
                 this.setState({ error: error });
             });
         }
@@ -21,7 +21,12 @@ const withErrorHandler = (WrappedComponent, axios) => {
         state = {
             error: null
         }
-        
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
+        }
+
         errorConfirmedHandler = () => {
             this.setState({ error: null });
         }
