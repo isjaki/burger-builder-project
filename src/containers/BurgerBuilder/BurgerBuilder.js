e4cbb0b47a807e3ec5ea5c9a59b52c46798e68d5
isjaki <usow.aleksey@yandex.ru>
2019-04-28 20:21:19 +0400

    fixed a bug with the order button in the BuildControls component, which was constantly disabled

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -38,9 +37,7 @@ class BurgerBuilder extends Component {
                 return sum + el;
             }, 0);
 
-        this.setState({
-            purchasable: sum > 0
-        });
+        return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -89,7 +86,7 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
                         price={this.props.price} 
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ingredients)}
                         ordered={this.purchaseHandler} />
                 </Aux>
             ); 
