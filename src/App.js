43f720f235be43f516b3d8a0b0a5bb1ef6f4c71d
isjaki <usow.aleksey@yandex.ru>
2019-07-25 00:06:35 +0300

    started storing auth data (token, expiration date and user id) in a local storage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { authCheckState } from './store/actions/index';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -9,6 +12,10 @@ import Logout from './containers/Auth/Logout/Logout';
 import Auth from './containers/Auth/Auth';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onAuthCheckState();
+  }
+
   render() {
     return (
       <div>
@@ -26,4 +33,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  onAuthCheckState: () => dispatch(authCheckState())
+});
+
+export default connect(null, mapDispatchToProps)(App);
